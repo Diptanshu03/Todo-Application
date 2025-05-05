762cf785d2fbca8a83036712d94154c4c8278e4d
Diptanshu Biswas <138897974+Diptanshu03@users.noreply.github.com>
2025-05-05 15:53:39 +0530

    Delete TodoApp/TodoApp directory

--- TodoApp/TodoApp/src/main/java/com/todo/TodoApp/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.todo.TodoApp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.todo.TodoApp.Model.Task;
-
-public interface TaskRepository extends JpaRepository<Task, Integer>{
-
-}
